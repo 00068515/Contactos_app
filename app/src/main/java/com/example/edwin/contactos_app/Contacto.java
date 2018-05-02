bc51d83a88314725c9205f9a0ffb6d6a3f47b5d6
Edwin Morales <edwin.morales9b@gmail.com>
2018-05-01 20:46:32 -0600

    Activity's creadas

--- app/src/main/java/com/example/edwin/contactos_app/Contacto.java
+++ app/src/main/java/com/example/edwin/contactos_app/Contacto.java
@@ -6,6 +6,45 @@ public class Contacto {
     private String correo;
     private int imagen;
 
-    
+    public Contacto(String nombre, int telefono, String correo, int imagen) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.imagen = imagen;
+    }
 
+    public Contacto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
 }
