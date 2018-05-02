4c369e7582cf4e07a4ec030dc0ec0876af024e1b
Edwin Morales <edwin.morales9b@gmail.com>
2018-05-01 20:19:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/edwin/contactos_app/Contacto.java
@@ -0,0 +1,11 @@
+package com.example.edwin.contactos_app;
+
+public class Contacto {
+    private String nombre;
+    private int telefono;
+    private String correo;
+    private int imagen;
+
+    
+
+}
